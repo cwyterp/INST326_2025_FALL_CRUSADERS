0266d54bf98f0c9d40af4617b9df13a6d7d10c6c
Jahnavi Vemuri <jvemuri@umd.edu>
2025-11-14 16:04:03 -0500

    added the storyline function

--- rpg_game.py
+++ rpg_game.py
@@ -1,5 +1,5 @@
 #Chris Section Boss Behavior 
-import random 
+import random
 aggbehavior = {"health": 30,
                 "phase1": [1, 1, 1, 1, 1, 2, 2, 3], 
                 "phase2" :[1, 1, 1, 1, 1, 2, 3, 3],
@@ -65,3 +65,18 @@ def special_boss_behavior(health, aggbehavior, defbehavior, passbehavior, player
         return boss_behavior(health, aggbehavior)
     else:
         return boss_behavior(health, passbehavior)
+    
+# Jahnavi's Section: Story Function
+alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+def storyline(filepath):
+    with open(filepath, 'r', encoding="UTF-8") as f:
+        for raw_line in f:
+            line = raw_line.strip()
+            print(line)
+            if line[-1] == "?":
+                choice = input("Choose your destiny.")
+                if choice not in alphabet:
+                    raise ValueError("Invalid choice. Enter a letter corresponding to your choice.")
+            else:
+                input("Press 'space' to continue...")
